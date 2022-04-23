12429e841481ff3454861dd4c187f9e182218186
Edson Henrique <edson_henrique@yahoo.com.br>
2022-04-23 16:17:17 -0300

    Atualizando

--- TED - 10/ted10_q2.py
+++ TED - 10/ted10_q2.py
@@ -1,10 +1,31 @@
 # Faça um algoritmo para ler 50 números e armazenar em um vetor VET, 
 # verificar e escrever se existem números repetidos no vetor VET e em que posições se encontram.
 
-n = 3
+# Ler os nº
+n = 8
 vet = [0] * n
 
 for i in range (n):
     vet [i] = input ("Digite o valor {atual} de {total}: ".format(atual = i+1, total = n))
+
 print(vet)
+
+# Mostra os nº repetidos
+rept = []
+lis = []
+
+for n in vet:
+    if n not in lis:
+        lis.append(n)
+    else:
+        rept.append(n)
+
+print("Nº repetidos: {}".format(rept))
+
+# Posição do nº
+lis = []
+for lis in rept:
+    if lis not in lis:
+        lis.append(lis)
+print("Posição: {}".format(lis))
 
