124eb0897bb3c667fe3d54242254d6326daad4e1
Edson Henrique <edson_henrique@yahoo.com.br>
2022-04-27 21:10:22 -0300

    atualizado

--- TED - 10/ted10_q1.py
+++ TED - 10/ted10_q1.py
@@ -1,7 +1,7 @@
 # Faça um algoritmo para ler 20 números e armazenar em um vetor. 
 # Após a leitura total dos 20 números, o algoritmo deve escrever esses 20 números lidos na ordem inversa.
 
-n = 5
+n = 20
 vet = [0] * n
 
 for i in range (n):
